690c691aafe3a142da1c435feb30c35dc9e75f70
kenwrites <28812088+kenwrites@users.noreply.github.com>
2020-04-17 21:36:12 -0400

    Adding LearnXinY code

---
+++ learnXinY/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    System.out.println("Hello World!");
+//	    System.out.println(
+//	            "Integer: " + 10 +
+//                "\nDouble: " + 3.14 +
+//                "\nBoolean: " + true
+
+//            System.out.print("Hello ");
+//            System.out.print(" World!");
+
+//        System.out.printf("pi = %.5f", Math.PI);
+
+//        var scanner = new Scanner(System.in);
+//        String name = scanner.next();
+//        byte numByte = scanner.nextByte();
+//        int numInt = scanner.nextInt();
+//        var numFloat = scanner.nextFloat();
+//        var numDouble = scanner.nextDouble();
+//        var bool = scanner.nextBoolean();
+    }
+}
