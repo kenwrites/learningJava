7bd634f76cc124c16c2e39131d73f003044bda25
kenwrites <28812088+kenwrites@users.noreply.github.com>
2020-04-19 20:37:28 -0400

    Adding stuff

--- learnXinY/src/com/company/Main.java
+++ learnXinY/src/com/company/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        /* Input / Output */
+
 //	    System.out.println("Hello World!");
 //	    System.out.println(
 //	            "Integer: " + 10 +
@@ -23,5 +26,51 @@ public class Main {
 //        var numFloat = scanner.nextFloat();
 //        var numDouble = scanner.nextDouble();
 //        var bool = scanner.nextBoolean();
+
+        /* Types and Variables */
+
+//        int fooint;
+//        int fooint1, fooint2, fooint3;
+//        int bar = 1;
+//        int barint1, barint2, barint3;
+//        barint1 = barint2 = barint3 = 1;
+//
+//        byte foobyte = 100; // 8-bit unsigned.
+//        int unsignedIntLessThan256 = 0xff & foobyte; // Always positive
+//        int signedInt = (int)foobyte; // Can be negative, if done via cast.
+//
+//        short fooshort = 10000;  // 16-bit signed
+//        fooshort = -10000;
+//
+//        int bazInt = 1000000; // 32-bit signed
+//        bazInt = -1000000;
+//
+//        long fooLong = -9223372036854775808L; // 64-bit signed.  End "L" denotes it's a long.  Otherwise compiler thinks int.
+//
+//        char fooChar = 'c'; // 16-bit unsigned Unicode character.  This is the only unsigned whole-number type.
+//
+//        float fooFloat = 234.5f; //  32-bit signed floating point. 'f' or 'F' denotes float.  Double by default
+//        double fooDouble = 123.4; // 64-bit signed floating point.
+//
+//        boolean fooBool = true;
+//
+//        // final vars can't be reassigned
+//        final int HOURS_I_WORK_PER_WEEK = 9001;
+//        // but they can be initialized later
+//        final double E;
+//        E = 2.71828;
+
+        String fooString = "My String is Here!";
+        String barString = "Here is a \nnew line.";
+        String bazString = "And here is a \t tab.";
+
+        System.out.println(fooString);
+        System.out.println(barString);
+        System.out.println(bazString);
+
+
+
+
+
     }
 }
